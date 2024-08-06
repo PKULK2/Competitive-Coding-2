d240db4c2ab54616ed905e6bb35b28e4d8ca81f4
PKULK2 <48229592+PKULK2@users.noreply.github.com>
2024-08-06 11:39:49 -0400

    Completed Problem2.java

---
+++ Problem2.java
@@ -0,0 +1,13 @@
+public class Knapsack {
+    public int knapsack(int[] weights, int[] values, int capacity) {
+        int n = weights.length;
+        int[] dp = new int[capacity + 1];
+
+        for (int i = 0; i < n; i++) {
+            for (int w = capacity; w >= weights[i]; w--) {
+                dp[w] = Math.max(dp[w], values[i] + dp[w - weights[i]]);
+            }
+        }
+
+        return dp[capacity];
+    }
